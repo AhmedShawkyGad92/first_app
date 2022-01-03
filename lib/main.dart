ac51283697ef38975bae4969d3a32166c5d84f6c
Ahmed SGad <Ahmed.SGad@mansourgroup.com>
2022-01-03 15:57:57 +0200

    Messenger_Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_import, duplicate_ignore
 // ignore: unused_import
-import 'package:first_app/home_screen.dart';
-import 'package:first_app/login_screen.dart';
+import 'home_screen.dart';
+import 'login_screen.dart';
 import 'package:flutter/material.dart';
+import 'messenger_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: MessengerScreen(),
     );
   }
 }
